cc3f4f52750331ed5400d01c4591ce50b2e6615d
div.Acc <kirillafafa@yandex.ru>
2024-06-24 12:47:39 +0300

    +DTO middleware

--- app/Telegram/Middleware/MyMiddleware.php
+++ app/Telegram/Middleware/MyMiddleware.php
@@ -2,21 +2,29 @@
 
 namespace App\Telegram\Middleware;
 
+use App\DTO\TelegramUserDTO;
+use App\Models\TelegramUser;
 use SergiX44\Nutgram\Nutgram;
 
 class MyMiddleware
 {
     public function __invoke(Nutgram $bot, $next): void
     {
-//        $telegramID = $bot->user()->id;
-//        $user = User::find($telegramID); // Запрос к бд на получение пользователя, если он есть!
-//        if (!$user) { // проверяем есть ли пользователь в бд, если нет то регаем, если есть, то пропускаем!
-//            $username = $bot->user()->username;
-//            $firstName = $bot->user()->first_name;
-//            $lastName = $bot->user()->last_name;
-//            //Передаем все данные в бд!
-//        }
-        $bot->sendMessage('Hello');
+        $uuid = $bot->user()->id;
+        /** @var TelegramUser $user */
+        $user = TelegramUser::find($uuid); // Запрос к бд на получение пользователя, если он есть!
+        if (!$user) { // проверяем есть ли пользователь в бд, если нет то регаем, если есть, то пропускаем!
+            $username = $bot->user()->username;
+            $firstName = $bot->user()->first_name;
+            $lastName = $bot->user()->last_name;
+            $telegramUserDTO = new TelegramUserDTO(
+                $uuid,
+                $firstName,
+                $lastName ?? null,
+                $username ?? null);
+            //Передаем все данные в бд!
+        }
+        $bot->sendMessage(" hello {$user}");
         $next($bot);
     }
 }
